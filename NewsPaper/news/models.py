982da3445451111643f2ad7ca49420aeadf9d9ca
Ant-on-git <denisenkoAA88@gmail.com>
2023-03-04 20:46:56 +0300

    created models for news app and added some entires in the database

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class Post(models.Model):
+    class Types(models.TextChoices):
+        article = 'статья', 'статья'
+        news = 'новость', 'новость'
+
+    type = models.CharField(max_length=7, choices=Types.choices, default=Types.article)
+    datetime_create = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    category = models.ManyToManyField('Category', through='PostCategory')
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return self.text[:124] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    datetime_create = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+
+class Author(models.Model):
+    rating = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def update_rating(self):
+        posts_rating = Post.objects.filter(author=self).aggregate(sum=Sum('rating'))['sum']
+        authors_comments_rating = Comment.objects.filter(user=self.user).aggregate(sum=Sum('rating'))['sum']
+        comments_rating_from_authors_posts = Comment.objects.filter(post__author=self).aggregate(sum=Sum('rating'))['sum']
+        self.rating = posts_rating * 3 + authors_comments_rating + comments_rating_from_authors_posts
+        self.save()
